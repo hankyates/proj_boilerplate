806fa112405a95147d9af3e9209f25b0763bbe86
Hank Yates <hankyates@gmail.com>
2016-03-06 16:39:17 -0800

    cleaning up build and dir structure

---
+++ src/index.js
@@ -0,0 +1,9 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('app')
+  );
+});
